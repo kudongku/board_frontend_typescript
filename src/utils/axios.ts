5f7ef2720e090abd6f675bdeddb96217bb0f3ef3
kudongku <kudongku@naver.com>
2024-09-19 00:43:47 +0900

    fix : axios request시에 token 실어서 보내기

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,10 +1,30 @@
-import axios from 'axios';
+import axios, {
+  AxiosInstance,
+  AxiosError,
+  InternalAxiosRequestConfig,
+} from 'axios';
 
-const instance = axios.create({
-  baseURL: 'http://localhost:8080/api',
+const instance: AxiosInstance = axios.create({
+  baseURL: 'http://localhost:8080/api', // 환경 변수 사용할 것.
+  timeout: 10000, // 타임아웃 설정 알아보기
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
+instance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
+    const token = localStorage.getItem('bearerToken');
+
+    if (token) {
+      config.headers['Authorization'] = `${token}`;
+    }
+    return config;
+  },
+  (error: AxiosError) => {
+    console.error('Request Error:', error.message);
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
