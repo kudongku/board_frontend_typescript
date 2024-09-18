1f61a1d447f9ef1de82693620e628dac15bd45d6
kudongku <kudongku@naver.com>
2024-09-18 23:45:51 +0900

    feat : navbar 생성

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+
+const Navbar: React.FC = ({}) => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('bearerToken');
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogout = (): void => {
+    localStorage.removeItem('bearerToken');
+    setIsLoggedIn(false);
+    window.location.href = '/';
+  };
+
+  return (
+    <div className="container mx-auto px-4 py-2 flex justify-start items-center">
+      <Link href="/" className="text-xl font-bold hover:text-gray-200 mr-4">
+        게시판
+      </Link>
+
+      {isLoggedIn ? (
+        <div>
+          <Link
+            href="/posts"
+            className="text-xl font-bold hover:text-gray-200 mr-4"
+          >
+            Posting
+          </Link>
+
+          <button
+            onClick={handleLogout}
+            className="text-xl font-bold hover:text-gray-200"
+          >
+            로그아웃
+          </button>
+        </div>
+      ) : (
+        <Link href="/login" className="text-xl font-bold hover:text-gray-200">
+          로그인
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
